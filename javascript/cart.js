a31128cbac8414c4e7c07057f1be46d46f7e59aa
EdricYeo117 <edricyeo.jr@gmail.com>
2023-11-23 03:42:01 +0800

    23/11/23 Added javascript functionaility to be able to add products to cart in shop.html Added said javascript to cart.js Tweaked styling for contact button in about.html Tweaked html <head> section to be more readable Added fontawesome icons to html pages

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,4 +1,3 @@
-
 window.onload = function () {
   /* Cart Javascript */
   let cartIcon = document.querySelector("#cart-icon");
@@ -21,4 +20,50 @@ window.onload = function () {
   } else {
     ready();
   }
+
+  function ready() {
+  // Adding to Cart - Cart
+  var addCart = document.getElementsByClassName('add-cart')
+  for (var i = 0; i < addCart.length; i++) {
+    var button = addCart[i]
+    button.addEventListener('click', addCartClicked);
+  }
+  }
+
+  // Add to Cart Function
+  function addCartClicked(event) {
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
+    var price = shopProducts.getElementsByClassName('price')[0].innerText;
+    var productImg = shopProducts.getElementsByClassName('product-img')[0].src;
+    addProductToCart(title, price, productImg);
+    updatetotal();
+  }
+  // Add Product to Cart Function
+  function addProductToCart(title, price, productImg) {
+    var cartShopBox = document.createElement('div');
+    cartShopBox.classList.add('cart-box');
+    var cartItems = document.getElementsByClassName('cart-content')[0];
+    var cartItemNames = cartItems.getElementsByClassName('cart-product-title');
+    for (var i = 0; i < cartItemNames.length; i++) {
+      if (cartItemNames[i].innerText == title) {
+        alert("This item is already added to the cart");
+        return;
+      }
+    }
+    // Cart Box Content, Making the indivdual product card in the cart
+    var cartBoxContent = `
+    <img src="${productImg}" alt="" class="cart-img">
+    <div class="detail-box">
+        <div class="cart-product-title">${title}</div>
+        <div class="cart-price">${price}</div>
+        <input type="number" value="1" class="cart-quantity">
+    </div>
+    <!-- Remove Cart -->
+    <i class='bx bxs-trash-alt cart-remove'></i>`;
+
+    cartShopBox.innerHTML = cartBoxContent;
+    cartItems.append(cartShopBox);
+}
 }
